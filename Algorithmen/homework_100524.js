1f3dd4a15f57f68ae7016bb9cde5804d2e353e8a
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-05-10 13:25:13 +0200

    Update homework_100524.js

--- Algorithmen/homework_100524.js
+++ Algorithmen/homework_100524.js
@@ -5,3 +5,19 @@ function sum(a,b,c) {
     return sum;
 }
 console.log(sum(a,b,c));
+
+// Задача 2. По двум заданным числам проверять является ли одно квадратом другого
+function checkSquareNum(a, b) {
+    let minNum = a;
+    let maxNum = b;
+    if (a > b) {
+        minNum = b;
+        maxNum = a;
+    }
+    if (maxNum / minNum === minNum) {
+        console.log(maxNum + " является квадратом " + minNum);
+    } else {
+        console.log(maxNum + " не является квадратом " + minNum);
+    }
+}
+checkSquareNum(2, 5);
